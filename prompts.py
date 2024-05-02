fb8f8b4aa05bb5966c8d82b1f653768aa9ac58d1
Femke Gelderblom <femke.gelderblom@sintef.no>
2024-05-02 08:55:07 +0200

    FEAT: Added fewshot prompt

--- prompts.py
+++ prompts.py
@@ -19,6 +19,8 @@ def get_context(prompt_id):
         return [{"role": "system", "content": PROMPT_IVa}]
     elif prompt_id == "Va":
         return [{"role": "system", "content": PROMPT_Va}]
+    elif prompt_id == "VIa":
+        return [{"role": "system", "content": PROMPT_VIa}]
     else:
         Exception("Prompt unknown")
 
@@ -211,6 +213,114 @@ Websites and digital products have privacy policies. I will give you a dataset o
 7. Policy Change: if and how users will be informed about changes to the privacy policy. Here are examples of some keywords, terms, and phrases that commonly identify "policy change" in privacy policies: “notify you about upcoming changes”, “we change this privacy policy from time to time”, “email notification of privacy policy changes”, “notification of changes”, “revisions to this policy”, “policy update alert”, “how we will inform you”, etc.
 8. Do Not Track: if and how Do Not Track signals for online tracking and advertising are honored. Examples of keywords, terms, and phrases that commonly reflect "Do Not Track" in privacy policies include: “you can switch off some cookies and similar tracking technologies”, “online tracking preferences”, “do not track”, “behavioral tracking opt-out”, “opting-out of tracking”, etc.
 9. International and Specific Audiences: practices that pertain only to a specific group of users (e.g., children, Europeans, or California residents). This category outlines specific provisions that apply exclusively to certain categories of users, such as children, or residents of Europe, or individuals living in California. The privacy policy includes clauses for these distinct user groups. Here are examples of some keywords, terms, and phrases that commonly identify international audiences or specific audiences in privacy policies: “child permissions”, “special protection of children's personal data”, “age-specific guidelines”, “kids profile”, “parental consent”, “children and teens”, “international users”, “regional laws”, “GDPR” (General Data Protection Regulation for European users), “CCPA” (California Consumer Privacy Act for California residents), “COPPA” (Children's Online Privacy Protection Act), “ CIPA” (Children’s Internet Protection Act in United States), “specific audience considerations”, “disclosure and international transfers”, “supplemental privacy disclosures for certain services and regions”, etc.
+
+I have a specific task for you: The user will give you a sentence from some privacy contents, and you tell them which category of practices this sentence may contain. Please only respond with the category.
+"""
+
+PROMPT_VIa = """
+1.	First Party Collection/Use.
+
+Policy segment: “We collect the following categories of personal information about you: name, email address, profile information, payment details and device IDs.”
+Classification: First Party Collection/Use
+
+Policy segment: “In order to provide personalized services, we process information including your usage data and preferences.”
+Classification: First Party Collection/Use
+
+Policy segment: “We may also collect sensitive personal information such as information about your racial or ethnic origin, health, precise geolocation, social security number. ”
+Classification: First Party Collection/Use
+
+2.	Third Party Sharing/Collection.
+
+Policy segment: “Our website uses third-party analytics services to help understand your usage of our services.”
+Classification: Third Party Sharing/Collection.
+
+Policy segment: “We collect personal Information from third parties whose services you use to access, pay for, or interact with our service.”
+Classification: Third Party Sharing/Collection.
+
+Policy segment: “We may collect personal information about you from other sources or service providers vendors, agents, and contractors that collect or provide personal information to us in connection with services they perform on our behalf.”
+Classification: Third Party Sharing/Collection.
+
+Policy segment: “Cookies from third-party advertisers are used on our platform to deliver targeted advertisements based on your browsing history.”
+Classification: Third Party Sharing/Collection.
+
+Policy segment: “Data we share with external partners includes your user profile and preferences to enhance your experience across partnered services.”
+Classification: Third Party Sharing/Collection.
+
+3.	User Choice/Control.
+
+Policy segment: “You can control what information we use to show you advertisements by visiting your advertisement settings.”
+Classification: User choice/control
+
+Policy segment: “If you wish to withdraw your consent at any time for any data processing we perform, you can do so through our user settings page under ´privacy control´.”
+Classification: User choice/control
+
+Policy segment: “You may choose to opt out of receiving behavioral advertisements, you will still see Advertisements, but they will not be behavioral advertisements.”
+Classification: User choice/control
+
+4.	 User Access, Edit, and Deletion.
+
+Policy segment: “You can request a copy of your personal information or download your data directly from your account settings.”
+Classification: User access, edit, and deletion
+
+Policy segment: “If you need to edit your information, you can do so by logging into your profile. For any modifications that are not available online, you may submit a request to our support team.”
+Classification: User access, edit, and deletion
+
+Policy segment: “You can delete your personal information that we have collected from or about you.”
+Classification: User access, edit, and deletion
+
+5.	Data Retention.
+Policy segment: “We retain personal information for as long as necessary to provide our service; to administer and operate our business; to research, analyze, and improve our services; to operate our ad supported subscription plan; for safety, security, and fraud prevention; and/or to enforce our Terms of Use.”
+Classification: data retention
+
+Policy segment: “Storage of personal information is based on many factors such as the nature of the personal information, and the need to defend or resolve current or anticipated legal claims.”
+Classification: data retention
+
+Policy segment: “We keep your personal information, including sensitive personal information, as long as we need it to provide our products, comply with legal obligations or protect our or other’s interests. We decide how long we need information on a case-by-case basis.”
+Classification: data retention
+
+6.	Data Security. 
+
+Policy segment: “We will share your personal information if the disclosure of information is reasonably necessary to Detect, prevent, or otherwise address fraud, security, or technical issues.”
+Classification: Data security
+
+Policy segment: “For safety, security, and fraud prevention we process personal information such as personal details, payment details, profile information, usage information, advertising information.”
+Classification: Data security
+
+Policy segment: “We follow appropriate technical and organizational measures to safeguard personal information, including regular security reviews and updates to our data breach procedures.”
+Classification: Data security
+
+7.	 Policy Change.
+
+Policy segment: “We use your information, like your email address, to notify you about upcoming changes to your subscription.”
+Classification: Policy Change
+
+Policy segment: “This privacy policy is subject to change from time to time. We will post revisions on our site and provide a policy update alert in our user interface.”
+Classification: Policy Change
+
+Policy segment: “In the event of significant changes in privacy policy, we will provide a more prominent notice via email notification.”
+Classification: Policy Change
+
+8.	 Do Not Track.
+Policy segment: “Our website respects 'Do Not Track' signals and does not track users who have set their browsers to decline tracking cookies.”
+Classification: Do Not Track
+
+Policy segment: “We do not currently respond to web browser “do not track" signals.”
+Classification: Do Not Track
+
+Policy segment: “You have right to opt-out of Targeted Advertising or location tracking.”
+Classification: Do Not Track
+
+9.	International and Specific Audiences.
+Policy segment: “Our services comply with the Data Protection Directive (also know as Directive 95/46/EC) to protect the privacy of our European users.”
+Classification: International and Specific Audiences
+
+Policy segment: “Parental consent is required before creating a kids profile on our platform, adhering to the Children's Online Privacy Protection Act (COPPA).”
+Classification: International and Specific Audiences
+
+Policy segment: “We may collect sensitive or personal information, as defined in U.S. Privacy Laws.”
+Classification: International and Specific Audiences
+
+
 
 I have a specific task for you: The user will give you a sentence from some privacy contents, and you tell them which category of practices this sentence may contain. Please only respond with the category.
 """
